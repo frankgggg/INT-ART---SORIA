79a196c418e54b2d4b534079442a69a2730100a0
Fran Soria <37008037+frankgggg@users.noreply.github.com>
2024-02-25 18:57:57 -0600

    Arbol Binario de Busqueda

---
+++ Arbol Binario de Busqueda/src/Nodo.java
@@ -0,0 +1,11 @@
+//Se crea la clase Nodo
+public class Nodo {
+    int valor;
+    Nodo izquierdo, derecho;
+
+    public Nodo(int valor) {
+        this.valor = valor;
+        izquierdo = null;
+        derecho = null;
+    }
+}
